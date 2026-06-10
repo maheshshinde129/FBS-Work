cd50fb0aa77f743c6552ca59b0411967f816ebe1
maheshshinde129 <shindemahi129@gmail.com>
2026-06-11 01:09:19 +0530

    Add files via upload

---
+++ C Programming/Assignment 0/area_of_circle.c
@@ -0,0 +1,10 @@
+// write a C program to find area of a circle
+
+#include<stdio.h>
+void main()
+{
+    float area;
+    float radius = 5;
+    area=3.14*radius*radius;
+    printf("area of circle %.2f",area);
+}
